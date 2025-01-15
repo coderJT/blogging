1b4a3686410612e78e57ac1e1c9e0e636786c98e
jtan0435 <jtan0435@student.monash.edu>
2025-01-15 23:42:49 +0800

    Customized login page and added shadcn ui.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,16 +1,58 @@
+"use client";
+
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import { login, signup } from "./actions";
+import { useState } from "react";
+import Link from "next/link";
 
 export default function LoginPage() {
+
+    const [error, setError] = useState<string | null>(null);
+
+    const handleLogin = async (formData: FormData) => {
+        setError(null);
+        const result = await login(formData);
+        if (result?.error) {
+            setError(result.error);
+        }
+    }
+    
     return (
-        <form>
-            <label htmlFor="email">Email: </label>
-            <input id="email" name="email" type="email" required></input>
+        <div className="min-h-screen flex items-center justify-center p-4">
+            <Card className="w-full max-w-lg">
+                <CardHeader>
+                    <CardTitle className="text-2xl font-bold">Login</CardTitle>
+                    <CardDescription className="text-sm text-gray-500">
+                        Enter your email and password to login to your account.
+                    </CardDescription>
+                </CardHeader>
 
-            <label htmlFor="password">Password:</label>
-            <input id="password" name="password" type="password" required></input>
+                <CardContent>
+                    <form>
+                        <div className="flex flex-col gap-1">
+                            <label htmlFor="email">Email: </label>
+                            <Input id="email" name="email" type="email" required />
+                        </div>
 
-            <button formAction={login}>Login</button>
-            <button formAction={signup}>Sign up</button>
-        </form>
+                        <div className="flex flex-col gap-1 mt-2">
+                            <label htmlFor="password">Password:</label>
+                            <Input id="password" name="password" type="password" required />
+                        </div>
+
+                        {error && <p className="text-red-500 mt-2">{error}</p>}
+
+                        <div className="flex gap-2 mt-6 mb-4">
+                            <Button variant="outline" formAction={handleLogin}>Login</Button>
+                            <Button variant="outline" formAction={signup}>Sign up</Button>
+                        </div>
+
+                        <Link href="/forgot-password" className="text-sm underline text-gray-400">Forgot Password</Link>
+
+                    </form>
+                </CardContent>
+            </Card>
+        </div>
     )
 }
