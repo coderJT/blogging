33743731cd9f65c754bcfe06faa31b4de177b622
coderJT <76200313+coderJT@users.noreply.github.com>
2025-01-17 11:00:32 +0800

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,6 +11,6 @@ export async function middleware(request: NextRequest) {
 // So that middleware does not run on routes that don't access supabase
 export const config = {
     matcher: [
-        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$|auth/signout).*)',
     ]
 }
